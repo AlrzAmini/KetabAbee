785a59a7194cbc6244834c378785daee47753a5c
AlirezaAmini <mranotmillion@gmail.com>
2021-12-14 21:03:46 +0330

    Create Interfaces , Repo For User

---
+++ KetabAbee.Application/Security/ImageValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace KetabAbee.Application.Security
+{
+    public static class ImageValidator
+    {
+        public static bool IsImage(this IFormFile value)
+        {
+            try
+            {
+                var img = System.Drawing.Image.FromStream(value.OpenReadStream());
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
