bfa16bb1f38137b517008d9eb6152d46d54913fd
AlirezaAmini <mranotmillion@gmail.com>
2021-12-15 00:35:24 +0330

    Create Register View

---
+++ KetabAbee.Web/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KetabAbee.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region Register
+
+        [Route("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        #endregion
+
+    }
+}
