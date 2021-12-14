6a7e7ae8d6b5f3399970c05e71b6712488660470
AlirezaAmini <mranotmillion@gmail.com>
2021-12-14 23:03:07 +0330

    UI works perfectly with our data

--- KetabAbee.Web/Controllers/HomeController.cs
+++ KetabAbee.Web/Controllers/HomeController.cs
@@ -6,11 +6,20 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using KetabAbee.Application.DTOs;
+using KetabAbee.Application.Interfaces.User;
 
 namespace KetabAbee.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUserService _userService;
+
+        public HomeController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         public IActionResult Index()
         {
             return View();
