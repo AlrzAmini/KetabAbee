48dbcdf26e1c1378b31d57f9750ea668520256ae
AlirezaAmini <mranotmillion@gmail.com>
2021-12-17 01:26:02 +0330

    Add User Panel Template

---
+++ KetabAbee.Web/Areas/UserPanel/Controllers/UserPanelBaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KetabAbee.Web.Areas.UserPanel.Controllers
+{
+    [Authorize]
+    [Area("UserPanel")]
+    public class UserPanelBaseController : Controller { }
+}
---
+++ KetabAbee.Web/Areas/UserPanel/Controllers/UserPanelController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KetabAbee.Web.Areas.UserPanel.Controllers
+{
+    public class UserPanelController : UserPanelBaseController
+    {
+        #region constructor
+
+
+
+        #endregion
+
+        #region Dashborad
+
+        [HttpGet("UserPanel/Dashboard")]
+        public async Task<IActionResult> Dashboard()
+        {
+            return View();
+        }
+
+        #endregion
+
+    }
+}
