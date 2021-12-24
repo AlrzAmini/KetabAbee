0e79116bb7ccbb675c634450cbc0a0a8b81b1fee
AlirezaAmini <mranotmillion@gmail.com>
2021-12-24 14:17:25 +0330

    Add Structure of users paging

---
+++ KetabAbee.Application/DTOs/Admin/User/FilterUsersViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KetabAbee.Application.DTOs.Paging;
+
+namespace KetabAbee.Application.DTOs.Admin.User
+{
+    public class FilterUsersViewModel : BasePaging
+    {
+        #region Props
+
+        public int? UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+
+        public List<Domain.Models.User.User> Users { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public FilterUsersViewModel SetUsers(List<Domain.Models.User.User> users)
+        {
+            Users = users;
+            return this;
+        }
+
+        public FilterUsersViewModel SetPaging(BasePaging paging)
+        {
+            PageNum = paging.PageNum;
+            TotalEntitiesCount = paging.TotalEntitiesCount;
+            StartPage = paging.StartPage;
+            EndPage = paging.EndPage;
+            PageCountAfterAndBefor = paging.PageCountAfterAndBefor;
+            Take = paging.Take;
+            Skip = paging.Skip;
+            TotalPages = paging.TotalPages;
+            return this;
+        }
+
+        #endregion
+    }
+}
