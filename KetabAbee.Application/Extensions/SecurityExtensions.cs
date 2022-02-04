dfac04fe1212d3cd972765167c76bdea742a41c6
AlirezaAmini <mranotmillion@gmail.com>
2022-02-04 20:11:27 +0330

    add sanitizer to project

---
+++ KetabAbee.Application/Extensions/SecurityExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Ganss.XSS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KetabAbee.Application.Extensions
+{
+    public static class SecurityExtensions
+    {
+        public static string Sanitizer(this string value)
+        {
+            var htmlSanitizer = new HtmlSanitizer
+            {
+                AllowDataAttributes = true,
+                KeepChildNodes = true
+            };
+            return htmlSanitizer.Sanitize(value);
+        }
+    }
+}
