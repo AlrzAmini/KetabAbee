a11d352380d0c59d042bbc4b18282a4722622c3c
AlirezaAmini <mranotmillion@gmail.com>
2022-02-04 22:29:14 +0330

    Footer links finished

--- KetabAbee.Web/wwwroot/js/custom.js
+++ KetabAbee.Web/wwwroot/js/custom.js
@@ -10,3 +10,36 @@
         message: decodeURI(text)
     });
 }
+$("#Send-Order-Guid").on("click", function () {
+    swal.fire({
+        title: '',
+        text: 'سفارش شما پس از ثبت بین 4 تا 6 روز کاری به دست شما می رسد',
+        icon: 'info',
+        confirmButtonText: 'باشه'
+    });
+});
+$("#Pay-Order-Way").on("click", function () {
+    swal.fire({
+        title: '',
+        text: 'برای پرداخت هزینه خرید خود میبایست کیف پول خود را در پروفایل کاربری به اندازه ای که نیاز دارید شارژ کرده و اقدام به خرید کنید',
+        icon: 'info',
+        confirmButtonText: 'باشه'
+    });
+});
+$("#Return-Product-Way").on("click", function () {
+    swal.fire({
+        title: '',
+        text: 'برای بازگردانی کالا کافیست شماره فاکتور خود و علت را برای ما از طریق پروفایل کاربری خریدار تیکت کنید',
+        icon: 'info',
+        confirmButtonText: 'باشه'
+    });
+});
+$("#Privecy--User").on("click", function () {
+    swal.fire({
+        title: '',
+        text: "کاربر عزیز اطلاعتی که شما در اختیار وبسایت ما قرار میدهید به هیچ عنوان در دسترس شخصی قرار نخواهد گرفت",
+        icon: 'success',
+        confirmButtonText: 'باشه'
+    });
+});
+
