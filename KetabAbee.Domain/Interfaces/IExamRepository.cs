f35fd37c1cd70a3e0e1b3464c12b8a3c3d1852d1
AlirezaAmini <mranotmillion@gmail.com>
2022-03-08 20:52:17 +0330

    Add exam repo and service

---
+++ KetabAbee.Domain/Interfaces/IExamRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KetabAbee.Domain.Models.Products.Exam;
+
+namespace KetabAbee.Domain.Interfaces
+{
+    public interface IExamRepository
+    {
+        Task<bool> AddExam(Exam exam);
+
+        Task<bool> RemoveExam(Exam exam);
+
+        Task<Exam> GetExamById(int examId);
+
+        Task<bool> UpdateExam(Exam exam);
+    }
+}
