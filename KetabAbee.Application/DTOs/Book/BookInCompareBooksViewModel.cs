275cbc0357fc8e3ad405a6a203368152e9485f1d
AlirezaAmini <mranotmillion@gmail.com>
2022-03-26 02:33:18 +0430

    Create Compare ViewModels

---
+++ KetabAbee.Application/DTOs/Book/BookInCompareBooksViewModel.cs
@@ -0,0 +1,28 @@
+﻿
+namespace KetabAbee.Application.DTOs.Book
+{
+    public class BookInCompareBooksViewModel
+    {
+        public string ImageName { get; set; }
+
+        public string ImageSavePath { get; set; }
+
+        public int Price { get; set; }
+
+        public string BookName { get; set; }
+
+        public string Writer { get; set; }
+
+        public string PublisherName { get; set; }
+
+        public string GroupTitle { get; set; }
+
+        public string CoverType { get; set; }
+
+        public int PagesCount { get; set; }
+
+        public string AgeRange { get; set; }
+
+        public string Abstract { get; set; }
+    }
+}
--- KetabAbee.Application/DTOs/Book/CompareBooksViewModel.cs
+++ KetabAbee.Application/DTOs/Book/CompareBooksViewModel.cs
@@ -9,6 +9,8 @@ namespace KetabAbee.Application.DTOs.Book
 {
     public class CompareBooksViewModel
     {
-        
+        public BookInCompareBooksViewModel FirstBook { get; set; }
+
+        public BookInCompareBooksViewModel SecondBook { get; set; } 
     }
 }
